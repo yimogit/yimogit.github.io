42c4415931ed3b44d30ea2b8d646c184037b443f
yimo <yimo@wikiglobal.com>
2024-12-07 16:02:08 +0800

    百度统计修改

---
+++ src/.vuepress/client.ts
@@ -0,0 +1,24 @@
+import { defineClientConfig } from 'vuepress/client'
+
+export default defineClientConfig({
+  enhance({ app, router, siteData }) {
+    /**
+   * 路由切换事件处理
+   */
+  router.beforeEach((to, from, next) => {
+    // console.log("切换路由", to.fullPath, from.fullPath);
+    //触发百度的pv统计
+    if (typeof _hmt  != "undefined") {
+      if (to.path) {
+        _hmt.push(["_trackPageview", to.fullPath]);
+        // console.log("上报百度统计", to.fullPath);
+      }
+    }
+
+    // continue
+    next();
+  });
+  },
+  setup() {},
+  rootComponents: [],
+})
